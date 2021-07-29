6ce3e7300296b2bbdf1a05a9791f9a8323cbbc9a
DhairyaHSinghvi <dhairya.singhvi@somaiya.edu>
2021-07-29 10:00:07 +0530

    Add project files.

---
+++ JenkinsSetUp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsSetUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
